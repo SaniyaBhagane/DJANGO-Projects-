0d30dcea31dbd8a582e3b74937d5e946b5e1b5f2
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-21 19:05:51 +0530

    implementation of views

--- chaihq/tweet/forms.py
+++ chaihq/tweet/forms.py
@@ -1,15 +1,53 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
         widgets = {
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'What\'s happening?'}),
-            'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': "What's happening?"
+            }),
+            'photo': forms.ClearableFileInput(attrs={
+                'class': 'form-control'
+            }),
         }
         labels = {
             'text': '',
             'photo': 'Add Photo (optional)',
         }
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
